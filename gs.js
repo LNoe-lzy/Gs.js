664a510520c3c642121a1b7c45e8c15f8fb92efa
LNoe-lzy <m15222505516@163.com>
2016-07-17 14:55:47 +0800

    增加height、width方法；添加全局接口Gsxhr；增加全局函数trim、ajax

--- gs.js
+++ gs.js
@@ -10,6 +10,12 @@
         return new gs(sel)
     };
     /*
+       全局ajax接口
+     */
+    window.Gxhr = Gsxhr = function () {
+       return new gsxhr();
+    };
+    /*
        定义版本号
      */
     Gs.version = '1.0';
@@ -58,7 +64,7 @@
         html: function (text) {
             if (arguments.length === 0) {
                 //去除多余的空格
-                return this.e[0].innerHTML.replace(/(^\s+)|(\s+$)/g, "");
+                return Gs.trim(this.e[0].innerHTML);
             } else if (arguments.length === 1) {
                 this.each(function (e) {
                     e.innerHTML = text;
@@ -139,12 +145,53 @@
          */
         css: function (arg) {
             if (typeof arg == 'string') {
-                return this.e[0].style[arg];
+                // return this.e[0].style[arg];
+                var obj = this.e[0];
+                //判断兼容性
+                if (obj.currentStyle) {
+                    //去除'px'
+                    return parseFloat(obj.currentStyle[arg]);
+                } else {
+                    //去除'px'
+                    return parseFloat(getComputedStyle(obj,false)[arg]);
+                }
             } else if (typeof arg == 'object') {
                 this.each(function (e) {
                     for (var name in arg) {
-                        e.style[name] = arg[name];
+                        if (typeof arg[name] === 'number') {
+                            e.style[name] = arg[name] + 'px';
+                        } else {
+                            e.style[name] = arg[name];
+                        }
                     }
+                });
+                return this;
+            }
+        },
+        /*
+           参数为空: 获取元素的高度;
+           参数不空: 设置元素的高度;
+         */
+        height: function (val) {
+            if (!val) {
+                return this.css('height');
+            } else {
+                this.css({
+                    height: val
+                });
+                return this;
+            }
+        },
+        /*
+         参数为空: 获取元素的宽度;
+         参数不空: 设置元素的宽度;
+         */
+        width: function (val) {
+            if (!val) {
+                return this.css('width');
+            } else {
+                this.css({
+                    width: val
                 });
                 return this;
             }
@@ -208,45 +255,136 @@
             this.each(function (e) {
                 var flag = true;
                 clearInterval(e.timer);
-                e.timer = setInterval(function(){
-                    for(var attr in obj){
+                e.timer = setInterval(function () {
+                    for(var attr in obj) {
                         var icur = 0;
-                        if(attr == 'opacity'){
-                            icur = Math.round(parseFloat(Gs.getStyle(e,attr))*100);
-                        }else{
-                            icur = parseInt(Gs.getStyle(e,attr));
+                        if (attr == 'opacity') {
+                            icur = Math.round(parseFloat(Gs.getStyle(e, attr)) * 100);
+                        } else {
+                            icur = parseInt(Gs.getStyle(e, attr));
                         }
                         var speed = 0;
                         speed = (obj[attr] - icur)/10;
                         speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-                        if(icur != obj[attr]){
+                        if (icur != obj[attr]) {
                             flag = false;
                         }
-                        if(attr == 'opacity'){
-                            e.style[attr] = (icur + speed)/100;
-                        }else{
+                        if (attr == 'opacity') {
+                            e.style[attr] = (icur + speed) / 100;
+                        } else {
                             e.style[attr] = icur + speed + "px";
                         }
                     }
-                    if(flag){
+                    if (flag) {
                         clearInterval(e.timer);
-                        if(fn){
+                        if (fn) {
                             fn();
                         }
                     }
-                },spd);
+                }, spd);
             });
+            return this;
         },
 
     };
 
+    //构建ajax对象
+    var gsxhr = function () {
+       if (window.XMLHttpRequest) {
+           return new XMLHttpRequest();
+       } else {
+           return new ActiveXObject("Microsoft.XMLHTTP");
+       }
+    };
+    /*
+       全局控制函数
+     */
+
     //获取元素的属性
-    Gs.getStyle = function (obj,attr) {
-        if(obj.currentStyle){
+    Gs.getStyle = function (obj, attr) {
+        if (obj.currentStyle) {
             return obj.currentStyle[attr];
-        }else{
+        } else {
             return getComputedStyle(obj,false)[attr];
         }
-    }
+    };
+    //去除空格
+    Gs.trim = function (str) {
+        return str.replace(/(^\s+)|(\s+$)/g, "")
+    };
+    //ajax
+    Gs.ajsx = function (data) {
+        var type = {
+            xml: "application/xml, text/xml",
+            html: "text/html",
+            script: "text/javascript, application/javascript",
+            json: "application/json, text/javascript",
+            text: "text/plain",
+            _default: "*/*"
+        };
+        var xhr = new gsxhr();
+
+        data.method = data.method.toUpperCase() || "GET";
+        data.datatype = data.datatype || "json";
+        data.asyn = data.asyn || true;
+
+        if (xhr) {
+            xhr.open(data.method, data.url, data.asyn);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.setRequestHeader("Accept", data.datatype && type[data.datatype] ? type[data.datatype] + ", */*" : type._default);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 1) {
+                    data.loading(xhr.response);
+                } else {
+                    if (xhr.readyState == 2) {
+                        data.loaded(xhr.response);
+                    } else {
+                        if (xhr.readyState == 3) {
+                            data.interactive(xhr.response);
+                        } else {
+                            if (xhr.readyState == 4) {
+                                if (xhr.status == 200) {
+                                    Gs.dataform(xhr, data.datatype);
+                                    data.success(xhr.response);
+                                    return xhr;
+                                } else {
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            try {
+                xhr.send(data.method === "POST" ? Gs.serialize(data.senddata) : null)
+            } catch (j) {
+                data.fail(xhr.response);
+            }
+        }
+    };
+
+    Gs.dataform = function (g, e) {
+        var d = g.getResponseHeader("content-type") || "",
+            c = e === "xml" || !e && d.indexOf("xml") >= 0,
+            f = c ? g.responseXML : g.responseText;
+        if (typeof f === "string") {
+            if (e === "json" || !e && d.indexOf("json") >= 0) {
+                // f = Oct.trim(f);
+                return window.JSON && window.JSON.parse ? window.JSON.parse(f) : (new Function("return " + f))()
+            }
+        }
+        return f
+    };
+
+    Gs.serialize = function (data) {
+        var key = [], val = [], pair = [], str = "";
+        for (var name in data) {
+            key.push(encodeURIComponent(name));
+            val.push(encodeURIComponent(data[name]));
+        }
+        for (var i = 0; i < key.length; i++) {
+            pair[i] = key[i] + "=" + val[i];
+        }
+        return str = pair.join("&");
+    };
 
 })(window);
