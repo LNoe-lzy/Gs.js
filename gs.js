9f5189bf9fcd13570de2d9bfd80c045a3302200c
LNoe-lzy <m15222505516@163.com>
2016-07-27 09:08:23 +0800

    修改了模块加载、定义、使用函数

--- gs.js
+++ gs.js
@@ -1,7 +1,7 @@
 ;(function (window) {
     /*
-       声明全局变量指定接口 'G'
-       sel: 用户指定的元素
+     声明全局变量指定接口 'G'
+     sel: 用户指定的元素
      */
     window.G = Gs = function (sel) {
         return new gs(sel)
@@ -389,83 +389,85 @@
     };
 
     //定义模块机制
-
-    /*
-     modeuleMap: 存储创建的模块
-     fileMap   : 存储文件的加载状态
-     noop      : 空函数
-     */
-    var moduleMap = {};
-    var fileMap = {};
-    var noop = function () {};
-    // 定义模块
-    Gs.define = function (name, dependencies, fn) {
-        /*
-         name         : 模块名
-         dependencies : 模块的依赖项
-         fn      : 模块的函数主体
-         */
-        if (!moduleMap[name]) {
-            var module = {
-                name: name,
-                dependencies: dependencies,
-                fn: fn
-            };
-            moduleMap[name] = module;
-        }
-        return moduleMap[name];
-    };
-    // 使用模块
-    Gs.use = function (name) {
-        // 存储要使用的模块
-        var module = moduleMap[name];
-
-        if (!module.entity) {
-            var args = [];
-            // 找到要依赖的模块
-            for (var i = 0; i < module.dependencies.length; i++) {
-                if (moduleMap[module.dependencies[i]].entity) {
-                    args.push(moduleMap[module.dependencies[i]].entity);
-                } else {
-                    args.push(this.use(module.dependencies[i]));
-                }
-            }
-            module.entity = module.fn.apply(noop, args);
-        }
-        return module.entity;
-    };
-    // 加载模块
-    Gs.require = function (pathArr, callback) {
-        for (var i = 0; i < pathArr.length; i++) {
-            var path = pathArr[i];
-
-            if (!fileMap[path]) {
-                var head = document.getElementsByTagName('head')[0];
-                var script = document.createElement('script');
-                script.type = 'text/javascript';
-                script.async = 'true';
-                script.src = './modules/' + path + '.js';
-                script.onload = function (argument) {
-                    fileMap[path] = true;
-                    head.removeChild(script);
-                    checkAllFiles();
+    Gs.module = {
+        moduleMap: [],
+        fileMap: [],
+        noop: function () {},
+        // 定义模块
+        define: function (name, dependencies, fn) {
+            /*
+              name         : 模块名
+              dependencies : 模块的依赖项
+              fn      : 模块的函数主体
+            */
+            var moduleMap = this.moduleMap;
+            if (!moduleMap[name]) {
+                var module = {
+                    name: name,
+                    dependencies: dependencies,
+                    fn: fn
                 };
-                head.appendChild(script);
+                moduleMap[name] = module;
             }
-        }
+            return moduleMap[name];
 
-        function checkAllFiles () {
-            var allLoaded = true;
+        },
+        // 加载模块
+        require: function (pathArr, callback) {
+            var fileMap = this.fileMap;
             for (var i = 0; i < pathArr.length; i++) {
-                if (!fileMap[pathArr[i]]) {
-                    allLoaded = false;
-                    break;
+                var path = pathArr[i];
+
+                if (!fileMap[path]) {
+                    var head = document.getElementsByTagName('head')[0];
+                    var script = document.createElement('script');
+                    script.type = 'text/javascript';
+                    script.async = 'true';
+                    script.src = './modules/' + path + '.js';
+                    script.onload = function (argument) {
+                        fileMap[path] = true;
+                        head.removeChild(script);
+                        checkAllFiles();
+                    };
+                    head.appendChild(script);
                 }
             }
-            if (allLoaded) {
-                callback();
+
+            function checkAllFiles () {
+                var allLoaded = true;
+                for (var i = 0; i < pathArr.length; i++) {
+                    if (!fileMap[pathArr[i]]) {
+                        allLoaded = false;
+                        break;
+                    }
+                }
+                if (allLoaded) {
+                    callback();
+                }
             }
+        },
+        // 使用模块
+        use: function (name) {
+            var noop = this.noop();
+            // 存储要使用的模块
+            var module = moduleMap[name];
+
+            if (!module.entity) {
+                var args = [];
+                // 找到要依赖的模块
+                for (var i = 0; i < module.dependencies.length; i++) {
+                    if (moduleMap[module.dependencies[i]].entity) {
+                        args.push(moduleMap[module.dependencies[i]].entity);
+                    } else {
+                        args.push(this.use(module.dependencies[i]));
+                    }
+                }
+                module.entity = module.fn.apply(noop, args);
+            }
+            return module.entity;
         }
     };
+
+    
 })(window);
 
