1a55140dd2d97f3067d3eecdf0d6ba11283c4850
LNoe-lzy <m15222505516@163.com>
2016-08-02 14:06:25 +0800

    修改部分方法细节。

--- gs.js
+++ gs.js
@@ -50,19 +50,15 @@
          */
         html: function (text) {
             if (arguments.length === 0) {
-                var args = [];
+                var a = [];
                 this.each(function (e) {
-                    args.push(Gs.trim(e.innerHTML));
+                    a.push(Gs.trim(e.innerHTML));
                 });
                 /*
                  选择单个对象: 返回值
                  选择多个对象: 返回数组
                  */
-                if (args.length === 1) {
-                    return args[0];
-                } else {
-                    return args;
-                }
+                return (a.length === 1 ? a[0] : a);
             } else if (arguments.length === 1) {
                 this.each(function (e) {
                     e.innerHTML = text;
@@ -79,15 +75,11 @@
                 console.error('选择对象无效: value()方法的有效对象为INPUT、TEXTAREA');
             }
             if (arguments.length === 0) {
-                var args = [];
+                var a = [];
                 this.each(function (e) {
-                    args.push(e.value);
+                    a.push(e.value);
                 });
-                if (args.length === 1) {
-                    return args[0];
-                } else {
-                    return args;
-                }
+                return (a.length === 1 ? a[0] : a);
             } else if (arguments.length === 1) {
                 this.each(function (e) {
                     e.value = val;
@@ -101,15 +93,11 @@
          */
         attr: function (arg) {
             if (typeof arg === 'string') {
-                var args = [];
+                var a = [];
                 this.each(function (e) {
-                    args.push(e.getAttribute(arg));
+                    a.push(e.getAttribute(arg));
                 });
-                if (args.length === 1) {
-                    return args[0];
-                } else {
-                    return args;
-                }
+                return (a.length === 1 ? a[0] : a);
             } else if (typeof arg === 'object') {
                 this.each(function (e) {
                     for (var name in arg) {
@@ -170,13 +158,16 @@
          */
         css: function (arg) {
             if (typeof arg === 'string') {
-                var obj = this.e[0];
-                //判断兼容性
-                if (obj.currentStyle) {
-                    return (obj.currentStyle[arg]);
-                } else {
-                    return (getComputedStyle(obj,false)[arg]);
-                }
+                var a = [];
+                this.each(function (e) {
+                    //判断兼容性
+                    if (e.currentStyle) {
+                        a.push(e.currentStyle[arg]);
+                    } else {
+                        a.push(getComputedStyle(e,false)[arg]);
+                    }
+                });
+                return (a.length === 1 ? a[0] : a);
             } else if (typeof arg === 'object') {
                 this.each(function (e) {
                     for (var name in arg) {
@@ -279,7 +270,7 @@
                 e.timer = setInterval(function () {
                     for(var attr in obj) {
                         var icur = 0;
-                        if (attr == 'opacity') {
+                        if (attr === 'opacity') {
                             icur = Math.round(parseFloat(Gs.getStyle(e, attr)) * 100);
                         } else {
                             icur = parseInt(Gs.getStyle(e, attr));
@@ -287,10 +278,10 @@
                         var speed = 0;
                         speed = (obj[attr] - icur)/10;
                         speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-                        if (icur != obj[attr]) {
+                        if (icur !== obj[attr]) {
                             flag = false;
                         }
-                        if (attr == 'opacity') {
+                        if (attr === 'opacity') {
                             e.style[attr] = (icur + speed) / 100;
                         } else {
                             e.style[attr] = icur + speed + "px";
@@ -432,7 +423,6 @@
     Gs.single = function (fn) {
         var result;
         return function () {
-            console.log(this);
             return result || (result = fn.apply(this, arguments));
         }
     };
